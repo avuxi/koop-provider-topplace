08ddc68318435aee9285e42ec74da0a5a52f854d
RaulJimenezOrtega <hhkaos@gmail.com>
2018-10-01 01:07:28 +0200

    Adding symbology templates folder

---
+++ symbologyDefinition/pois.js
@@ -0,0 +1,153 @@
+const drawingInfo = {
+  "renderer": {
+    "visualVariables": [
+      {
+        "type": "transparencyInfo",
+        "field": "rank",
+        "stops": [
+          {
+            "value": 58,
+            "transparency": 70
+          },
+          {
+            "value": 78,
+            "transparency": 0
+          }
+        ]
+      }
+    ],
+    "authoringInfo": {
+      "visualVariables": [
+        {
+          "type": "transparencyInfo",
+          "minSliderValue": 57,
+          "maxSliderValue": 99
+        }
+      ]
+    },
+    "type": "uniqueValue",
+    "field1": "cat",
+    "uniqueValueInfos": [
+      {
+        "value": "restaurant",
+        "label": "restaurant",
+        "symbol": {
+          "color": [
+            237,
+            81,
+            81,
+            255
+          ],
+          "size": 6,
+          "angle": 0,
+          "xoffset": 0,
+          "yoffset": 0,
+          "type": "esriSMS",
+          "style": "esriSMSCircle",
+          "outline": {
+            "color": [
+              153,
+              153,
+              153,
+              64
+            ],
+            "width": 0.75,
+            "type": "esriSLS",
+            "style": "esriSLSSolid"
+          }
+        }
+      },
+      {
+        "value": "sights",
+        "label": "sights",
+        "symbol": {
+          "color": [
+            20,
+            158,
+            206,
+            255
+          ],
+          "size": 6,
+          "angle": 0,
+          "xoffset": 0,
+          "yoffset": 0,
+          "type": "esriSMS",
+          "style": "esriSMSCircle",
+          "outline": {
+            "color": [
+              153,
+              153,
+              153,
+              64
+            ],
+            "width": 0.75,
+            "type": "esriSLS",
+            "style": "esriSLSSolid"
+          }
+        }
+      },
+      {
+        "value": "nightlife",
+        "label": "nightlife",
+        "symbol": {
+          "color": [
+            167,
+            198,
+            54,
+            255
+          ],
+          "size": 6,
+          "angle": 0,
+          "xoffset": 0,
+          "yoffset": 0,
+          "type": "esriSMS",
+          "style": "esriSMSCircle",
+          "outline": {
+            "color": [
+              153,
+              153,
+              153,
+              64
+            ],
+            "width": 0.75,
+            "type": "esriSLS",
+            "style": "esriSLSSolid"
+          }
+        }
+      },
+      {
+        "value": "shopping",
+        "label": "shopping",
+        "symbol": {
+          "color": [
+            158,
+            85,
+            156,
+            255
+          ],
+          "size": 6,
+          "angle": 0,
+          "xoffset": 0,
+          "yoffset": 0,
+          "type": "esriSMS",
+          "style": "esriSMSCircle",
+          "outline": {
+            "color": [
+              153,
+              153,
+              153,
+              64
+            ],
+            "width": 0.75,
+            "type": "esriSLS",
+            "style": "esriSLSSolid"
+          }
+        }
+      }
+    ]
+  }
+}
+
+
+
+module.exports = drawingInfo
